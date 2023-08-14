dbe6819af6ca9cdc0a96bb7136eff4cdfca2d3a8
Tamás Zentai <zentaitamas@gmail.com>
2023-08-14 21:22:21 +0100

    organization and initial server setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+app.get('/', (req ,res) => {
+    res.send('First game');
+});
+
+io.on("connection", (socket) => {
+    // ...
+});
+
+httpServer.listen(3000);
